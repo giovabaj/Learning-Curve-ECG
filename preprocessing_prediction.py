f4f38c2d3f9882fdb0dc288a8baccfb7a4025936
giovabaj <giovanni.baj2@gmail.com>
2025-05-16 15:06:34 +0200

    Create out dir if not present

--- preprocessing_prediction.py
+++ preprocessing_prediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import os
+from os.path import join
 from multiprocessing import Pool
 import argparse
 
@@ -10,7 +11,7 @@ from utils import filter_ecgs
 
 def main():
     # parsing arguments:
-    parser = argparse.ArgumentParser(description='ECG - makedataset')
+    parser = argparse.ArgumentParser(description='ECG - make dataset')
     parser.add_argument('--n_samples', type=int, help='Number of samples to extract')
     parser.add_argument('--path_df', type=str, help='Path to the csv dataframe')
     parser.add_argument('--path_signals', type=str, help='Path to signals')
@@ -18,21 +19,18 @@ def main():
     parser.add_argument('--out_path', type=str, default='data/prediction', help='Output path')
     args = parser.parse_args()
     n_samples = args.n_samples
-    path_df = args.path_df
-    wfdir = args.path_signals
     freq_resample = args.freq_resampling
-    dsdir = args.out_path
     random_seed = 42
 
     # Importing dataframe
-    df = pd.read_csv(path_df, encoding="iso-8859-1", low_memory=False)
+    df = pd.read_csv(args.path_df, encoding="iso-8859-1", low_memory=False)
     df = df.drop(df[(df["FA"] == 0) & (df["duration"] < 1825)].index, axis=0)  # Drop no-AF patients with FUP < 5 years
     df = df.sample(int(1.2*n_samples), random_state=random_seed)  # Randomly sample patients
     print("dataframe loaded")
     print("   checking if all exams are present")
     exams = df["ExamID"].values.astype(int)
     labels = df["FA"].values
-    filenames = np.array([os.path.join(wfdir, str(indx) + '.npy') for indx in exams])
+    filenames = np.array([join(args.path_signals, str(indx) + '.npy') for indx in exams])
     ncores = 10  # number of cores, e.g. 8
     pool = Pool(ncores)
     # create a list of booleans corresponding to whether each file is in path or not.
@@ -46,7 +44,7 @@ def main():
     filenames_exist = filenames_exist[:n_samples]  # select only the required number of exams
     labels = labels[:n_samples]
 
-    # Resample the signal to the required frqeuency
+    # Resample the signal to the required frequency
     n_points_resample = 10 * freq_resample  # number of time points after resampling
     ecg_data = np.empty([len(exams_exist), 12, n_points_resample], dtype='float32')
     print("   Start")
@@ -60,8 +58,9 @@ def main():
     ecg_data = filter_ecgs(ecg_data, fs=freq_resample, low_cut=0.67, high_cut=100)
 
     # saving ECG matrix
-    filename = os.path.join(dsdir, f'X_{n_samples}_12Leads_{freq_resample}hz')
-    print(f'   Saving ECGs to file {filename}')
+    filename = join(args.out_path, f'data_{n_samples}_{freq_resample}hz')
+    print(f'   Saving ECGs to {filename}')
+    os.makedirs(args.out_path, exist_ok=True)
     np.savez_compressed(filename, ecg_data, labels, exams_exist)
 
 
