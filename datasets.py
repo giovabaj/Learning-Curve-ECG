6ed2c4fbbaeb2cbdb879a2f38a867bc7eb4610c4
giovabaj <giovanni.baj2@gmail.com>
2025-05-16 16:02:36 +0200

    Update docstrings

--- datasets.py
+++ datasets.py
@@ -3,8 +3,15 @@ from torch.utils.data import Dataset
 
 
 class EcgDataset(Dataset):
-    """Dataset definition for the binary classification problem"""
+    """
+    PyTorch Dataset for ECG signals.
 
+    Args:
+        ecg_data (np.ndarray or tensor): 2D array of shape (n_samples, signal_length).
+        labels (np.ndarray or tensor): 1D array of binary labels (0 or 1).
+        augment (bool): If True, apply Gaussian noise augmentation.
+        std_augm (float): Standard deviation of the Gaussian noise used for augmentation.
+    """
     def __init__(self, ecg_data, labels, augment=False, std_augm=.00002):
         super().__init__()
         self.augment = augment
